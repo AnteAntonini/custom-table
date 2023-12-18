617e7d1272b563d38ecc58b456e7a25b7d41e0ea
AnteAntonini <aantonini@combis.hr>
2023-12-18 14:57:31 +0100

    implement custom hook for fetching data

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return data;
+};
