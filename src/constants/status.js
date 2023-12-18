173d7c7804c4b21bde4410bab7d097196689cace
AnteAntonini <aantonini@combis.hr>
2023-12-18 14:57:40 +0100

    add status constants

---
+++ src/constants/status.js
@@ -0,0 +1,8 @@
+export const STATUS = {
+  0: { info: "N/A", color: "var(--Shade-8)" },
+  1: { info: "Operational", color: "var(--Category-5)" },
+  2: { info: "Not operational", color: "var(--Category-6)" },
+  3: { info: "Partial", color: "var(--Category-7)" },
+  4: { info: "Pending", color: "var(--Category-7)" },
+  5: { info: "Stopped", color: "var(--Shade-4)" },
+};
